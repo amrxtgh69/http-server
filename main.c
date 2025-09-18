8fdb3fc31eb223dc1d0914fa9da9e7759825113f
amrxtgh <ghimireamrit619@gmail.com>
2025-09-18 15:51:54 +0545

    Hell yeah! this now can serve html file

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
@@ -7,11 +8,43 @@
 
 #define PORT 8080
 
+void send_file(int client_fd, const char *filename) {
+  FILE *file = fopen(filename, "r");
+  if (file == NULL) {
+    char *not_found = 
+        "HTTP/1.1 404 Not Found\r\n"
+        "Content-Type: text/plain\r\n"
+        "Content-Length: 13\r\n\r\n"
+        "404 Not Found";
+    write(client_fd, not_found, strlen(not_found));
+    return;
+  }
+  fseek(file, 0, SEEK_END);
+  long filesize = ftell(file);
+  rewind(file);
+
+  char *file_content = malloc(filesize);
+  fread(file_content, 1, filesize, file);
+  fclose(file);
+
+  //response header 
+  char header[256];
+  snprintf(header, sizeof(header),
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/html\r\n"
+            "Content-Length: %ld\r\n\r\n", filesize);
+  write(client_fd, header, strlen(header));
+  write(client_fd, file_content, sizeof(file_content));
+
+  free(file_content);
+}
+
+
 int main() {
   int server_fd, client_fd;
   struct sockaddr_in address;
   int addrlen = sizeof(address);
-  char buffer[1024] = {0};
+  char buffer[2048] = {0};
   
   server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -25,15 +58,20 @@ int main() {
 
   while(1) {
     client_fd = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
-    read(client_fd, buffer, 1024);
+    int valread = read(client_fd, buffer, sizeof(buffer) - 1);
+    buffer[valread] = '\0';
     printf("Request:\n%s\n", buffer);
 
-    char *response = 
-            "HTTP/1.1 200 OK\n"
-            "Content-Type: text/plain\n"
-            "Content-Length: 30\n\n"
-            "A monk in the cloud";
-    write(client_fd, response, strlen(response));
+    if (strncmp(buffer, "GET /index ", 11) == 0) {
+      send_file(client_fd, "index.html");
+    } else {
+      char *response = 
+         "HTTP/1.1 404 Not Found\r\n"
+         "Content-Type: text/plain\r\n"
+         "Content-Length: 13\r\n\r\n"
+         "404 Not Found";
+      write(client_fd, response, strlen(response));
+    }
     close(client_fd);
   }
   return 0;
